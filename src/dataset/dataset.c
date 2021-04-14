4b6ec56bfc4d9d2700d4797f8c23450f8dd38168
James Clarke <hello@jclarke.dev>
2021-04-14 12:20:57 +0100

    Add stubs for new dataset module

---
+++ src/dataset/dataset.c
@@ -0,0 +1,5 @@
+//
+// Created by James Clarke on 11/04/2021.
+//
+
+#include "include/dataset.h"
---
+++ src/dataset/include/dataset.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "stdio.h"
+
+typedef struct Dataset Dataset;
+typedef struct MapNode MapNode;
+typedef struct Link Link;
+
+struct MapNode
+{
+    unsigned int id;
+    float lat;
+    float lon;
+};
+
+struct Link
+{
+    unsigned int start;
+    unsigned int end;
+    unsigned int way;
+    float length;
+    float veg;
+    float arch;
+    float land;
+};
+
+struct Dataset
+{
+    int numNodes;
+    MapNode *nodes;
+    int numLinks;
+    Link *links;
+};
+
+/**
+ * Parses a .map file
+ * @param file A file stream to be read from
+ * @return A pointer to a dataset struct, null if error occurs.
+ */
+Dataset *ingest(FILE *file);
+
+/**
+ * Frees memory used by a dataset
+ * @param dataset The dataset to be freed
+ * @return A status code, 1 if successful
+ */
+int deleteDataset(Dataset *dataset);
