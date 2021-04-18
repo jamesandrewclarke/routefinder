7baceb778524fb1127b7f50a339492a88c688ac4
James Clarke <hello@jclarke.dev>
2021-04-18 22:37:04 +0100

    Simplify integration test to make it easier to add future test cases

--- tests/integration_test.c
+++ tests/integration_test.c
@@ -8,44 +8,49 @@
 #include "convert.h"
 #include <stdio.h>
 
+static Dataset *dataset;
+static Graph *graph;
+static Route *route;
+
 void setUp()
 {
+    FILE *fp = fopen("../../data/Final_Map.map", "r");
+    TEST_ASSERT_NOT_NULL_MESSAGE(fp, "Test file could not be opened.\n");
+
+    dataset = ingest(fp);
+    fclose(fp);
+    graph = datasetToGraph(dataset);
 }
 
 void tearDown()
 {
+    deleteDataset(dataset);
+    deleteGraph(graph);
+    deleteRoute(route);
 }
+
+// The following cases are (simple) solutions produced by the program that we know to be correct
+// We check against these for regression testing
 
 void integration_test(void)
 {
-    FILE *fp = fopen("../../data/Final_Map.map", "r");
-    Dataset *result = ingest(fp);
+    route = dijkstra_shortestRoute(graph, 7, 10);
 
-    Graph *graph = datasetToGraph(result);
+    TEST_ASSERT_EQUAL_INT(4, route->numVertices);
+    TEST_ASSERT_EQUAL_FLOAT(29.596424, route->cost);
+    TEST_ASSERT_EQUAL_INT_ARRAY(((unsigned int[]){7,8,11,10}), route->nodes, 4);
+    TEST_ASSERT_NOT_NULL(route);
+}
 
-    Route *route = dijkstra_shortestRoute(graph, 7, 500);
-    if (route != NULL)
-    {
-        printf("Total Nodes: %i\n", route->numVertices);
-        printf("Length: %f\n", route->cost);
-    } else
-    {
-        fprintf(stderr, "No route found.\n");
-    }
 
-    printf("Route: ");
-    for (int i = 0; i < route->numVertices; i++)
-    {
-        printf("%i, ", route->nodes[i]);
-    }
+void integration_test2(void)
+{
+    route = dijkstra_shortestRoute(graph, 1000, 1005);
 
-    printf("\n");
-
-    deleteDataset(result);
-    fclose(fp);
-
-    deleteRoute(route);
-    deleteGraph(graph);
+    TEST_ASSERT_EQUAL_INT(6, route->numVertices);
+    TEST_ASSERT_EQUAL_FLOAT(41.317493, route->cost);
+    TEST_ASSERT_EQUAL_INT_ARRAY(((unsigned int[]){1000, 1001, 1002, 1003, 1004, 1005}), route->nodes, 6);
+    TEST_ASSERT_NOT_NULL(route);
 }
 
 int main()
@@ -53,6 +58,7 @@ int main()
     UNITY_BEGIN();
 
     RUN_TEST(integration_test);
+    RUN_TEST(integration_test2);
 
     return UNITY_END();
 }
