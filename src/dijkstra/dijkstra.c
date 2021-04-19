9a42dcc0af81de478aad49a450b1ee7fd469c1b8
James Clarke <hello@jclarke.dev>
2021-04-19 17:51:30 +0100

    Refactor the Dijkstra module in to multiple functions

--- src/dijkstra/dijkstra.c
+++ src/dijkstra/dijkstra.c
@@ -7,9 +7,59 @@
 #include <stdlib.h>
 #include <float.h>
 
-Route *dijkstra_shortestRoute(const Graph *graph, const unsigned int start, const unsigned int end)
+typedef struct DijkstraResult DijkstraResult;
+
+struct DijkstraResult
 {
-    if (end > graph->numVertices) return NULL;
+    float *dist;
+    unsigned int *prev;
+};
+
+/**
+ * Produces an array of a path to the 'end' node from a given DijkstraResult
+ * @param result The DijkstraResult to find the path from.
+ * @param end The ID of the 'end' node.
+ * @return A Route struct.
+ */
+Route *getRoute(const DijkstraResult *result, const unsigned int end)
+{
+    if (result->prev[end] == UNDEFINED)
+    {
+        return NULL; // the end vertex is not reachable
+    }
+
+    Route *route = malloc(sizeof(Route));
+    route->cost = result->dist[end];
+
+    unsigned int next = result->prev[end];
+    int vertices = 1;
+    while (next != UNDEFINED)
+    {
+        next = result->prev[next];
+        vertices++;
+    }
+
+    unsigned int *nodes = calloc(vertices, sizeof(unsigned int *));
+    next = end;
+    for (int i = 0; i < vertices; i++)
+    {
+        nodes[vertices - 1 - i] = next;
+        next = result->prev[next];
+    }
+    route->numVertices = vertices;
+    route->nodes = nodes;
+
+    return route;
+}
+
+/**
+ * Finds the shortest path between the 'start' node and every other node in the graph.
+ * @param graph The graph containing all other nodes
+ * @param start The ID of the start node.
+ * @return A DijkstraResult struct containing both a distance and precedence array.
+ */
+DijkstraResult *dijkstra(const Graph *graph, const unsigned int start)
+{
     // Pseudocode from https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
     float *dist = calloc(graph->numVertices, sizeof(float));
     unsigned int *prev = calloc(graph->numVertices, sizeof(unsigned int));
@@ -34,9 +84,6 @@ Route *dijkstra_shortestRoute(const Graph *graph, const unsigned int start, cons
         const Node minNode = extractMin(Q);
         const unsigned int u = minNode.id;
 
-        // If the end node was at the front of the queue, terminate the search
-        if (u == end) break;
-
         Edge *ref = (graph->vertices + u)->head;
         // Traverse the linked list to find the closest node
         while (ref != NULL)
@@ -54,41 +101,28 @@ Route *dijkstra_shortestRoute(const Graph *graph, const unsigned int start, cons
         }
 
     }
-    // The algorithm is complete, now we have to record the route
-
-    if (prev[end] == UNDEFINED)
-    {
-        free(dist);
-        free(prev);
-        deleteQueue(Q);
-        return NULL; // the end vertex is not reachable
-    }
-
-    Route *route = malloc(sizeof(Route));
-    route->cost = dist[end];
-
-    unsigned int next = prev[end];
-    int vertices = 1;
-    while (next != UNDEFINED)
-    {
-        next = prev[next];
-        vertices++;
-    }
-
-    unsigned int *nodes = calloc(vertices, sizeof(unsigned int *));
-    next = end;
-    for (int i = 0; i < vertices; i++)
-    {
-        nodes[vertices - 1 - i] = next;
-        next = prev[next];
-    }
-    route->numVertices = vertices;
-    route->nodes = nodes;
-
-    free(dist);
-    free(prev);
 
     deleteQueue(Q);
+
+    DijkstraResult *result = malloc(sizeof(DijkstraResult));
+    result->prev = prev;
+    result->dist = dist;
+
+    return result;
+}
+
+Route *dijkstra_shortestRoute(const Graph *graph, const unsigned int start, const unsigned int end)
+{
+    if (end > graph->numVertices) return NULL;
+
+    DijkstraResult *result = dijkstra(graph, start);
+
+    // The algorithm is complete, now we have to record the route
+    Route *route = getRoute(result, end);
+
+    free(result->dist);
+    free(result->prev);
+    free(result);
     return route;
 }
 
